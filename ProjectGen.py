a4263a6b73daec0a8f74d6fd3bcffc16a6de1895
Nyjun <numen.lrs@gmail.com>
2019-01-28 19:09:19 +0100

    Config file parsing

--- ProjectGen.py
+++ ProjectGen.py
@@ -7,12 +7,43 @@ targetFolders = []
 rtti = False
 noExc = False
 vectExt = ""
+AllowedVectorExtensions = ["AVX", "AVX2", "IA32", "SSE", "SSE2", "SSE3",\
+ "SSSE3", "SSE4.1", "NEON", "MXU"]
 
 # Parsing Config File
-
-
-
-
+tmp = ""
+state = 0
+read = False
+file = open("Config.txt", "r")
+for line in file:
+	for c in line:
+		if c == '=':		# Start recording value
+			state += 1
+			read = True
+		elif c == ';':		# End recording + store value
+			read = False
+			if state == 1 and tmp != "":
+				clientNames.append(tmp)
+				targetFolders.append(os.path.join(".",tmp))
+			if state == 2:
+				rtti = tmp == "On"
+			if state == 3:
+				noExc = tmp == "on"
+			if state == 4:
+				if tmp in AllowedVectorExtensions:
+					vectExt = tmp
+			tmp = ""
+		elif c == ',':		# Store multiple values
+			read = False
+			if state == 1 and tmp != "":
+				clientNames.append(tmp)
+				targetFolders.append(os.path.join(".",tmp))
+				tmp = ""
+				read = True
+		else:
+			if read == True:
+				tmp += c
+				
 
 # Completion with defaults when necessary
 if len(clientNames) == 0:
